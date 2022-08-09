e95b76900060d65e3c9c261b633f36cbcf3d2143
sebasolr <seba.olr@gmail.com>
2022-08-09 12:22:12 -0400

    archivos modificado

--- server.js
+++ server.js
@@ -4,16 +4,12 @@ const uuid = require('uuid')
 const fs = require('fs').promises
 const router = express.Router()
 //const bodyParser =require('body-parser')
-
 const app = express()
 app.use(express.static('public'))
-
 // app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(bodyParser.json());
-
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
 function getForm(req) {
   return new Promise((res, rej) => {
     let str = ''
@@ -48,7 +44,6 @@ const mostrarRoommetes = async function () {
 // ___________________________________________________________________________
 const crearGastos = async function (nuevoGasto,roommates,monto,debe) {
   let archivo_db = await fs.readFile('db.json', 'utf8')// 1. Leemos el contenido del archivo 'db.json'
-
   archivo_db = JSON.parse(archivo_db)// 2. Transformamos su contenido (string) a un objeto de JS
   archivo_db.roommates.map(function(dato){if(dato.nombre==roommates){dato.recibe += monto}})
   archivo_db.roommates.map(function(dato){if(dato.nombre){dato.debe += debe}})
@@ -66,30 +61,22 @@ const mostrarHistorialGastos = async function () {
   return archivo_db.gastos
 }
 //const ChangeRecibe = async function (nameRoomie,valorFinal){
-
-
 app.post('/roommates', async (req, res) => {
   const resp = await axios.get('https://randomuser.me/api')
   const datos = resp.data
   //console.log(resp.data) 
   const nombre = `${datos.results[0].name.first} ${datos.results[0].name.last}`
-
   //obtener id
   const id_unico = uuid.v4()
-
   const nuevoRoommates = {
     id: id_unico,
     nombre: nombre,
     debe: 0,
     recibe: 0
   }
-
   await crearRoommates(nuevoRoommates)
-  
-
   res.json({})
 })
-
 app.get('/roommates', async (req, res) => {
   let roommates = await mostrarRoommetes()
   res.json({ roommates })
@@ -112,67 +99,86 @@ app.post('/gastos', async (req, res) => {
     descripcion: descripcion,
     monto: monto
   } 
-  let archivo_db = await fs.readFile('db.json', 'utf8')// 1. Leemos el contenido del archivo 'db.json'
-  archivo_db = JSON.parse(archivo_db)// 2. Transformamos su contenido (string) a un objeto de JS
-  const holi = function (){
-    let x= 0;
-    for(let i=0;i<=100;i++){
-    if(archivo_db.roommates[i] === undefined){
-      return x
-    }else{x+=1}
+let archivo_db = await fs.readFile('db.json', 'utf8')// 1. Leemos el contenido del archivo 'db.json'
+archivo_db = JSON.parse(archivo_db)// 2. Transformamos su contenido (string) a un objeto de JS
+const holi = function (){
+  let x= 0;
+  for(let i=0;i<=100;i++){
+  if(archivo_db.roommates[i] === undefined){
+    return x
+  }else{x+=1}
   }}
-  let debe = monto/holi()
-  
-  await crearGastos(nuevoGasto,roommates,monto,debe)
-   
-  //se debe traer a base, tranformar a objeto ,bscar posicion,modificarlar,se vuelve a texto,se sobre escribe el archivo
-
-
-  
-  res.json({})
+let debe = monto/holi()
+await crearGastos(nuevoGasto,roommates,monto,debe)
+//se debe traer a base, tranformar a objeto ,bscar posicion,modificarlar,se vuelve a texto,se sobre escribe el archivo
+res.json({})
 })
-
 app.get('/gastos', async (req, res) => {
   let gastos = await mostrarHistorialGastos()
   res.json({ gastos })
 })
 async function eliminarUsuario(id){
-  let archivo_db = await fs.readFile('db.json','utf-8') 
-  archivo_db = JSON.parse(archivo_db)
-  archivo_db.gastos.splice(id,1)
-  archivo_db = JSON.stringify(archivo_db)
-  await fs.writeFile('db.json', archivo_db, 'utf8')
-
+  let archivo_db = await fs.readFile('db.json','utf-8') //traemos la bd
+  archivo_db = JSON.parse(archivo_db)//json
+  archivo_db.gastos = archivo_db.gastos.filter(g => g.id !=id);
+  archivo_db.gastos.map(function(dato){if(dato.nombre==roommates){dato.recibe += monto}})
+  archivo_db = JSON.stringify(archivo_db);
+  await fs.writeFile('db.json', archivo_db, 'utf8');
 }
-/* async function modificarHistorial (nombre, descripcion, monto){
-  let archivo_db = await fs.readFile('db.json','utf-8') 
-  archivo_db = JSON.parse(archivo_db)
-  archivo_db.gastos.map(function(dato){if(dato.nombre==nombre){dato.monto = monto, dato.descripcion = descripcion}})
+async function editarHistorial(id,nombre,desc,monto){
+  let archivo_db = await fs.readFile('db.json','utf-8') //traemos la bd
+  archivo_db = JSON.parse(archivo_db)//json
+
+  // obtenemos el gasto a cambiar
+  const gasto = archivo_db.gastos.find(g => g.id == id)
+  
+  // calculamos la diferencia en gastos (15000, 10000)
+  const diferenciaGasto = gasto.monto - monto
+
+  // nos traemos al antiguo y nuevo creador
+  const antiguoCreador = archivo_db.roommates.find(r => r.nombre == gasto.roommates)
+  const nuevoCreador = archivo_db.roommates.find(n => n.nombre == nombre)
+  
+  // actualizamos los "recibe"
+  antiguoCreador.recibe -= gasto.monto
+  nuevoCreador.recibe += monto
+  
+  // le cambiamos sus atributos al gasto
+  gasto.roommates = nombre
+  gasto.descripcion = desc
+  gasto.monto = monto
+
+  // actualizamos los "debe"
+  console.log(diferenciaGasto);
+  for (let roomie of archivo_db.roommates) {
+    roomie.debe -= diferenciaGasto/(archivo_db.roommates.length)
+  }
+
+  // por último, guardamos el archivo
   archivo_db = JSON.stringify(archivo_db)
   await fs.writeFile('db.json', archivo_db, 'utf8')
-} */
+}
 app.put('/gastos',async (req,res) =>{
-
   const gastosEditados = await getForm(req)
+
   const nombreEditado = gastosEditados.roommates;
   const descripcionEditado = gastosEditados.descripcion;
   const montoEditado = gastosEditados.monto;
-  /* modificarHistorial(nombreEditado,descripcionEditado,montoEditado) */
+
+  editarHistorial(req.query.id,nombreEditado,descripcionEditado,montoEditado)
   console.log(req.query.id, nombreEditado,descripcionEditado,montoEditado);
   res.send('holi')
 })
 app.delete('/gastos', async (req, res) => {
   
   const eliminado = req.query.id;
+  console.log(eliminado);
   eliminarUsuario(eliminado)
   res.send('holi')
 })
-
-
  app.get('*', (req, res) => {
      res.send('Página aún no implementada')
  });
-
 app.listen(3000, function () {
   console.log(`Servidor corriendo en http://localhost:${3000}/`)
 })
